3d6131ed02ee59602b06dd76896e3258c11535a3
nitheshsivakumar <nitheshsivakumar19@gmail.com>
2025-02-24 20:45:32 -0600

    updated alb module

--- alb/main.tf
+++ alb/main.tf
@@ -3,8 +3,8 @@ resource "aws_lb" "application_load_balancer" {
   name                       = "${var.project_name}-${var.environment}-alb"
   internal                   = false
   load_balancer_type         = "application"
-  security_groups            = [var.alb_security_group.id]
-  subnets                    = [var.public_subnet_az1.id, var.public_subnet_az2.id]
+  security_groups            = [var.alb_security_group_id]
+  subnets                    = [var.public_subnet_az1_id, var.public_subnet_az2_id]
   enable_deletion_protection = false
 
   tags = {
